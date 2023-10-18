49ca102878b8a56ae6de1402ba02376ab4a82c34
alfanalamin <alfanalamin123@gmail.com>
2023-10-19 02:13:57 +0700

    update UI and func button navbar

--- lib/presentation/components/common/bottom_navbar.dart
+++ lib/presentation/components/common/bottom_navbar.dart
@@ -1,71 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:manhatan_project/common/color.dart';
+import 'package:manhatan_project/presentation/providers/button_provider.dart';
+import 'package:provider/provider.dart';
 
-import '../../../common/color.dart';
-
-class BottomNavBar extends StatefulWidget {
+class BottomNavBar extends StatelessWidget {
   final int selectedIndex;
   final ValueChanged<int> onItemTapped;
-
+  final VoidCallback onKonsultasi;
   const BottomNavBar({
-    super.key,
+    Key? key,
     required this.selectedIndex,
     required this.onItemTapped,
-  });
+    required this.onKonsultasi,
+  }) : super(key: key);
 
-  @override
-  State<BottomNavBar> createState() => _BottomNavBarState();
-}
-
-class _BottomNavBarState extends State<BottomNavBar> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Stack(
-          children: [
-            BottomNavigationBar(
-              currentIndex: widget.selectedIndex,
-              onTap: widget.onItemTapped,
-              selectedItemColor: BaseColors.primary500,
-              unselectedItemColor: BaseColors.neutral500,
-              showSelectedLabels: true,
-              showUnselectedLabels: true,
-              items: [
-                const BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/images/home.png'),
-                  ),
-                  label: 'Home',
-                ),
-                const BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/images/lamp-on.png'),
-                  ),
-                  label: 'Varises',
-                ),
-                BottomNavigationBarItem(
-                    icon: Image.asset(
-                      'assets/images/Button.png',
-                      width: 24,
-                      height: 24,
-                    ),
-                    label: 'Konsultasi'),
-                const BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/images/archive-book.png'),
-                  ),
-                  label: 'Artikel',
-                ),
-                const BottomNavigationBarItem(
-                  icon: ImageIcon(
-                    AssetImage('assets/images/gallery-favorite.png'),
-                  ),
-                  label: 'Galeri',
-                ),
-              ],
+    final bottomNavBarProvider = Provider.of<ButtonProvider>(context);
+
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      currentIndex: bottomNavBarProvider.currentIndex,
+      onTap: (index) {
+        if (index != 2) {
+          bottomNavBarProvider.currentIndex = index;
+          onItemTapped(index);
+        } else {
+          onKonsultasi();
+        }
+      },
+      selectedItemColor: BaseColors.primary600,
+      unselectedItemColor: BaseColors.neutral500,
+      showSelectedLabels: true,
+      showUnselectedLabels: true,
+      items: [
+        BottomNavigationBarItem(
+          icon: ImageIcon(
+            const AssetImage(
+              'assets/images/home.png',
             ),
-          ],
+            color: bottomNavBarProvider.currentIndex == 0
+                ? BaseColors.primary600
+                : BaseColors.neutral500,
+            size: 24.w,
+          ),
+          label: 'Home',
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(
+            const AssetImage('assets/images/lamp-on.png'),
+            color: bottomNavBarProvider.currentIndex == 1
+                ? BaseColors.primary600
+                : BaseColors.neutral500,
+            size: 24.w,
+          ),
+          label: 'Varises',
+        ),
+        BottomNavigationBarItem(
+          icon: SizedBox(
+            width: 24.w,
+            height: 24.w,
+          ),
+          label: 'Konsultasi',
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(
+            const AssetImage('assets/images/archive-book.png'),
+            color: bottomNavBarProvider.currentIndex == 3
+                ? BaseColors.primary600
+                : BaseColors.neutral500,
+            size: 24.w,
+          ),
+          label: 'Artikel',
+        ),
+        BottomNavigationBarItem(
+          icon: ImageIcon(
+            const AssetImage('assets/images/gallery-favorite.png'),
+            color: bottomNavBarProvider.currentIndex == 4
+                ? BaseColors.primary600
+                : BaseColors.neutral500,
+            size: 24.w,
+          ),
+          label: 'Galeri',
         ),
       ],
     );
