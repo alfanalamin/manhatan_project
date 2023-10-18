e7f61d609ae302cb50b19333bca999900936a008
alfanalamin <alfanalamin123@gmail.com>
2023-10-19 03:05:44 +0700

    fix splash screen

--- lib/presentation/pages/splash_screen.dart
+++ lib/presentation/pages/splash_screen.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:manhatan_project/presentation/components/common/skeleton_home_page.dart';
+import 'package:manhatan_project/presentation/pages/main_page.dart';
 
 import '../../common/color.dart';
-import 'home_page.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -21,8 +20,7 @@ class _SplashScreenState extends State<SplashScreen> {
       () => Navigator.pushReplacement(
         context,
         MaterialPageRoute(
-          builder: (context) =>  const HomePage(
-          ),
+          builder: (context) => const MainPage(),
         ),
       ),
     );
