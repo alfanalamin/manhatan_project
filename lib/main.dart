ab56eabd190aa8f3161bc4de6d88da274833ba52
alfanalamin <alfanalamin123@gmail.com>
2023-10-19 03:08:29 +0700

    fix main func

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,32 @@
+import 'package:fimber/fimber.dart';
 import 'package:flutter/material.dart';
-import 'package:manhatan_project/presentation/pages/splash_screen.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:intl/date_symbol_data_local.dart';
+import 'package:intl/intl.dart';
+import 'package:manhatan_project/injection.dart' as di;
+import 'package:manhatan_project/presentation/providers/about_provider.dart';
+import 'package:manhatan_project/presentation/providers/button_provider.dart';
+import 'package:manhatan_project/presentation/providers/galery_provider.dart';
+import 'package:manhatan_project/presentation/providers/home_provider.dart';
+import 'package:manhatan_project/presentation/providers/konsultasi_provider.dart';
+import 'package:manhatan_project/presentation/providers/varises_provider.dart';
+import 'package:provider/provider.dart';
 
-void main() {
+
+import 'presentation/pages/splash_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  di.init();
+  Intl.defaultLocale = 'id';
+  initializeDateFormatting();
+  await di.locator.allReady();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+  Fimber.plantTree(DebugTree());
   runApp(const MyApp());
 }
 
@@ -11,15 +36,36 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      builder: (context, child) {
+        return MultiProvider(
+          providers: [
+            ChangeNotifierProvider(
+              create: (_) => di.locator<ButtonProvider>(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => di.locator<HomeProvider>(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => di.locator<VarisesProvider>(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => di.locator<AboutProvider>(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => di.locator<GaleryProvider>(),
+            ),
+            ChangeNotifierProvider(
+              create: (_) => di.locator<KonsultasiProvider>(),
+            ),
+          ],
+          child: const MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: SplashScreen(),
+          ),
+        );
+      },
     );
   }
 }
-
