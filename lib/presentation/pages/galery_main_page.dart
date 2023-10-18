c6308154d476aa23ce3dbecef4225e93899d8290
alfanalamin <alfanalamin123@gmail.com>
2023-10-19 03:01:45 +0700

    fix galery data from api

--- lib/presentation/pages/galery_main_page.dart
+++ lib/presentation/pages/galery_main_page.dart
@@ -1,129 +1,202 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_masonry_view/flutter_masonry_view.dart';
-
-import '../../common/app_theme.dart';
-import '../../common/color.dart';
-import '../components/common/bottom_navbar.dart';
-import 'home_page.dart';
-import 'konsultasi_page.dart';
-import 'varises_main_page.dart';
+import 'package:get_it/get_it.dart';
+import 'package:manhatan_project/common/app_shared_preferences.dart';
+import 'package:manhatan_project/common/app_theme.dart';
+import 'package:manhatan_project/common/color.dart';
+import 'package:manhatan_project/common/state_enum.dart';
+import 'package:manhatan_project/data/models/galery_response.dart';
+import 'package:manhatan_project/presentation/components/common/skeleton_galery.dart';
+import 'package:manhatan_project/presentation/pages/about_main_page.dart';
+import 'package:manhatan_project/presentation/pages/main_page.dart';
+import 'package:manhatan_project/presentation/providers/galery_provider.dart';
+import 'package:provider/provider.dart';
 
 class GaleryMainPage extends StatefulWidget {
-  GaleryMainPage({Key? key});
-
-  final List<String> _items = [
-    'assets/images/grid1.png',
-    'assets/images/grid2.png',
-    'assets/images/grid3.png',
-    'assets/images/grid4.png',
-    'assets/images/grid5.png',
-  ];
+  const GaleryMainPage({super.key});
 
   @override
   State<GaleryMainPage> createState() => _GaleryMainPageState();
 }
 
 class _GaleryMainPageState extends State<GaleryMainPage> {
-  final int _selectedIndex = 0;
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      Provider.of<GaleryProvider>(
+        context,
+        listen: false,
+      ).doGetGalery();
+    });
+  }
 
-  void _onItemTapped(int index) {
-    if (index == 0) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const HomePage()),
-      );
-    } else if (index == 1) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const VarisesMainPage()),
-      );
-    } else if (index == 2) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const KonsultasiPage()),
-      );
-    } else if (index == 3) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => const VarisesMainPage()),
-      );
-    } else if (index == 4) {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => GaleryMainPage()),
-      );
-    }
+  void _showImageDialog(String image, String name) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          elevation: 0,
+          backgroundColor: Colors.transparent,
+          contentPadding: EdgeInsets.zero,
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height * 0.6,
+                  child: Ink(
+                    color: Colors.transparent,
+                    child: CachedNetworkImage(
+                      imageUrl: image,
+                      fit: BoxFit.cover,
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Text(
+                name,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        automaticallyImplyLeading: false,
         elevation: 0,
         shadowColor: Colors.transparent,
         backgroundColor: Colors.white,
         title: Padding(
-          padding: const EdgeInsets.fromLTRB(20, 10, 20, 16),
+          padding: const EdgeInsets.fromLTRB(10, 10, 10, 16),
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
               Text(
-                'Gallery',
-                style: AppTheme.appTextTheme.regulerNoneMedium,
+                'Galeri',
+                style: AppTheme.appTextTheme.regulerNoneMedium!.copyWith(
+                  color: BaseColors.neutral950,
+                ),
               ),
               Row(
                 children: [
-                  Container(
-                    width: 60,
-                    height: 35,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(20),
-                      color: Colors.white,
-                      border: Border.all(
-                        color: BaseColors.neutral200,
+                  InkWell(
+                    onTap: () {
+                      GetIt.I<AppSharedPreferences>().toggleLang();
+                      Navigator.pushReplacement(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const MainPage(),
+                        ),
+                      );
+                    },
+                    child: Container(
+                      width: 60,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                        color: Colors.white,
+                        border: Border.all(
+                          color: BaseColors.neutral200,
+                        ),
+                      ),
+                      child: Row(
+                        children: [
+                          Image.asset(
+                            'assets/images/flag_${GetIt.I<AppSharedPreferences>().getLang()}.png',
+                            width: 30,
+                            height: 30,
+                          ),
+                          const SizedBox(width: 8),
+                          Text(
+                            GetIt.I<AppSharedPreferences>()
+                                .getLang()
+                                .toUpperCase(),
+                            style: AppTheme.appTextTheme.xSmallNoneMedium!
+                                .copyWith(color: BaseColors.neutral950),
+                          ),
+                        ],
                       ),
                     ),
-                    child: Row(
-                      children: [
-                        Image.asset(
-                          'assets/images/id Indonesia.png',
-                          width: 30,
-                          height: 30,
-                        ),
-                        const SizedBox(width: 8),
-                        Text(
-                          'ID',
-                          style: AppTheme.appTextTheme.xSmallNoneMedium,
-                        ),
-                      ],
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const AboutMainPage()),
+                      );
+                    },
+                    child: const Icon(
+                      Icons.info_outline_rounded,
+                      color: Colors.black,
+                      size: 35,
                     ),
-                  ),
-                  const SizedBox(width: 8),
-                  const Icon(
-                    Icons.info_outline_rounded,
-                    color: Colors.black,
-                    size: 35,
-                  ),
+                  )
                 ],
               ),
             ],
           ),
         ),
       ),
-      body: ListView(
-        children: [
-          MasonryView(
-            listOfItem: widget._items,
-            numberOfColumn: 2,
-            itemBuilder: (item) {
-              return Image.asset(item);
-            },
-          ),
-        ],
-      ),
-      bottomNavigationBar: BottomNavBar(
-        selectedIndex: _selectedIndex,
-        onItemTapped: _onItemTapped,
+      body: Consumer<GaleryProvider>(
+        builder: (context, provider, child) {
+          if (provider.galeryState == RequestState.loading ||
+              provider.galeryState == RequestState.empty) {
+            return const SkeletonGalery();
+          }
+          final gallery = provider.galeryResponse?.galery ?? [];
+          gallery.isNotEmpty ? const SizedBox(height: 10) : const SizedBox();
+          return ListView(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(20, 10, 0, 16),
+                child: MasonryView(
+                  listOfItem: gallery,
+                  numberOfColumn: 2,
+                  itemBuilder: (item) {
+                    final galleryItem = item as Galery;
+                    final image = galleryItem.imagedir;
+                    final name = galleryItem.name;
+                    return GestureDetector(
+                      onTap: () {
+                        _showImageDialog(image ?? '', name ?? '');
+                      },
+                      child: ClipRRect(
+                        child: CachedNetworkImage(
+                          imageUrl: image ?? '',
+                          fit: BoxFit.cover,
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
